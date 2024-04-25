760746c50a5874f3d982c4928349a7d97fbeb72e
Rony Mateo <rony.mateo@ext.abcdin.cl>
2024-04-24 21:21:51 -0500

    add routing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Habit Tracker</h1>
-      <Link href="/dashboard">Dashboard</Link>
+      <h1>Dashboard page</h1>
     </main>
   );
 }
