3ee84ebc3cb9e4cd754c2586f096463866cd2e6b
Rony Mateo <rony.mateo@ext.abcdin.cl>
2024-04-25 23:05:27 -0500

    add prisma

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+import { GetServerSideProps } from "next";
+import prisma from "../../lib/prisma";
+
+export const getData = async () => {
+  const users = await prisma.users.findMany();
+  console.log(users);
+  return users;
+};
+
+export default async function Home() {
+  const users = await getData();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Dashboard page</h1>
+      <h1>{JSON.stringify(users)}</h1>
     </main>
   );
 }
